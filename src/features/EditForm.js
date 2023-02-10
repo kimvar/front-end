7a9fc4c0e8fe602c369237bc51685c6e301b2bf2
Ismail <katirciismail29@gmail.com>
2023-02-10 15:48:24 +0300

    feat/edit check file with tckn

--- src/features/EditForm.js
+++ src/features/EditForm.js
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { Box, Button, Flex } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
+import axios from "axios";
 
 function EditForm() {
   const { register, handleSubmit } = useForm();
@@ -9,9 +10,14 @@ function EditForm() {
   const [ifrmSrc, setIfrmSrc] = useState("");
   const [isIfrmVisible, setIfrmVisible] = useState(false);
 
-  const onSubmit = ({ code }) => {
+  const onSubmit = async ({ tckn }) => {
+    const res = await axios.get(
+      `https://api.jotform.com/form/230393262424956/submissions?apiKey=38c1f731467c6e32e36eca2c5c&filter={"q77":${tckn}`
+    );
+
+    const { id } = res.data.content[0];
     setIfrmVisible(true);
-    setIfrmSrc(`https://jotform.com/edit/${code}`);
+    setIfrmSrc(`https://jotform.com/edit/${id}`);
   };
 
   return (
@@ -20,7 +26,7 @@ function EditForm() {
         <>
           <form onSubmit={handleSubmit(onSubmit)}>
             <Flex flexDirection="column" gap={15} maxWidth={500}>
-              <label htmlFor="code">Kayıt No:</label>
+              <label htmlFor="code">Depremzede TC No:</label>
               <Input required {...register("code")} />
               <Button type="submit" colorScheme="blue">
                 Düzenle

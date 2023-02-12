aa53f3b92d48fcb41b4d7099e82bbbb435850940
Ismail <katirciismail29@gmail.com>
2023-02-12 12:54:10 +0300

    (feat): firebase analitics config

--- src/App.js
+++ src/App.js
@@ -10,6 +10,31 @@ import {
 } from "@chakra-ui/react";
 import EditForm from "features/EditForm";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTAybHY_w0A9kVQZjlB7H9H35-1bDGGbY",
+
+  authDomain: "kimvar-dev-3b957.firebaseapp.com",
+
+  projectId: "kimvar-dev-3b957",
+
+  storageBucket: "kimvar-dev-3b957.appspot.com",
+
+  messagingSenderId: "418574655888",
+
+  appId: "1:418574655888:web:e6bc6c2c821fcaf35c9315",
+
+  measurementId: "G-R1C261371H",
+};
+
+const app = initializeApp(firebaseConfig);
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+  getAnalytics(app);
+}
+
 function App() {
   return (
     <div>

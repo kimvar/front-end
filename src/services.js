523c4fd7e89d0a6985f4226b1205214d57c20db4
Can Küçükyılmaz <53147473+ivanfuzuli@users.noreply.github.com>
2023-02-22 23:10:51 +0300

    feat: add table support

---
+++ src/services.js
@@ -0,0 +1,30 @@
+import axios from "./@axios";
+
+/**
+ * @todo utils klasörü olması lazım
+ */
+export const filtersToQueryparams = (filters) => {
+  const filtersReduced = filters
+    .map((item) => {
+      return {
+        [item.id]: item.value,
+      };
+    })
+    .reduce((acc, val) => {
+      return { ...acc, ...val };
+    }, {});
+
+  let filterQuery = "";
+  if (filtersReduced && Object.keys(filtersReduced).length > 0) {
+    filterQuery = `&filters=${JSON.stringify(filtersReduced)}`;
+  }
+
+  return filterQuery;
+};
+
+export const getReportsFn = async ({ id, limit, offset, filters }) => {
+  const response = await axios.get(
+    `/report?identityNumber=${id}&limit=${limit}&offset=${offset}${filters}`
+  );
+  return response.data;
+};

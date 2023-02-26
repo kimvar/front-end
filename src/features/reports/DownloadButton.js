923ea89054ec8314ac7c2b4bb683c99f084d062a
gsimsekPersonal <gokhan3345@gmail.com>
2023-02-26 23:03:16 +0300

    fix :: download reports endpoint changed

--- src/features/reports/DownloadButton.js
+++ src/features/reports/DownloadButton.js
@@ -1,23 +1,30 @@
-import { Button } from "@chakra-ui/react";
+import { useRef } from "react";
+import { Button, useToast } from "@chakra-ui/react";
 import { user } from "@utils";
 import { useState } from "react";
-import { registerReportRequest } from "services";
+import { downloadReports } from "services";
 
 const DownloadButton = ({ setErrorMessage }) => {
   const [isDownloading, setDownloading] = useState(false);
 
+  const toast = useToast();
+  const toastIdRef = useRef();
+
   const handleDownload = async () => {
+    const { tckn } = user.credantials;
+
     setDownloading(true);
     setErrorMessage(null);
+
     try {
-      const _token = await registerReportRequest();
-      if (_token) {
-        window.open(
-          `${process.env.REACT_APP_API_URL}downloadReport?type=excel&token=${_token}&identityNumber=${user.credantials.tckn}`
-        );
-      } else {
-        setErrorMessage("Beklenmedik bir hata oluştu.");
-      }
+      const res = await downloadReports({ tckn });
+      toastIdRef.current = toast({
+        title: "Başarılı!",
+        description: res.message,
+        status: "success",
+        isClosable: true,
+        position: "top-right",
+      });
     } catch (err) {
       setErrorMessage(err.message);
     } finally {
